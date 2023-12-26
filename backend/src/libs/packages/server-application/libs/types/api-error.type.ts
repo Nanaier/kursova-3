d60978400fd75d415e59b1bc3631673ee122d3f4
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-12-26 22:38:21 +0200

    Updates for deployment added

--- backend/src/libs/packages/server-application/libs/types/api-error.type.ts
+++ backend/src/libs/packages/server-application/libs/types/api-error.type.ts
@@ -5,8 +5,6 @@ import {
   type ValidationError,
 } from '~/libs/exceptions/exceptions.js';
 import { type HTTPError } from '~/libs/packages/http/http.js';
-import { type ChatError } from '~/packages/chats/chats.js';
-import { type JournalError } from '~/packages/journal-entries/journal-entries.js';
 import { type UserError } from '~/packages/users/users.js';
 
 type APIError =
@@ -14,8 +12,6 @@ type APIError =
   | ValidationError
   | HTTPError
   | AuthError
-  | ChatError
-  | JournalError
   | UserError;
 
 export { type APIError };
