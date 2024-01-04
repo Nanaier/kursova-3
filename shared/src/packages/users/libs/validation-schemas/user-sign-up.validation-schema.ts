490e7c144854116f84dcf538dc570c701fdf13e3
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2024-01-04 03:29:05 +0200

    Added password regex

--- shared/src/packages/users/libs/validation-schemas/user-sign-up.validation-schema.ts
+++ shared/src/packages/users/libs/validation-schemas/user-sign-up.validation-schema.ts
@@ -4,10 +4,17 @@ import { UserValidationMessage } from '../enums/enums.js';
 import { type UserSignUpRequestDto } from '../types/types.js';
 
 const userSignUp = joi.object<UserSignUpRequestDto, true>({
-  password: joi.string().trim().required().messages({
-    'any.required': UserValidationMessage.PASSWORD_REQUIRED,
-    'string.empty': UserValidationMessage.PASSWORD_REQUIRED,
-  }),
+  password: joi
+    .string()
+    .trim()
+    .required()
+    .min(8)
+    .pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/)
+    .messages({
+      'string.min': 'Password must be at least 8 characters',
+      'string.pattern.base':
+        'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+    }),
   username: joi.string().trim().required().messages({
     'any.required': UserValidationMessage.NAME_REQUIRED,
     'string.empty': UserValidationMessage.NAME_REQUIRED,
